7e546850803fad6721db31c996d64b830288930e
aStesh891 <ann.steshhh@gmail.com>
2021-02-14 21:13:35 +0200

    adding POJOs. Closes #2.

---
+++ src/main/java/guru/spring/petclinic/model/Pet.java
@@ -0,0 +1,35 @@
+package guru.spring.petclinic.model;
+
+import java.time.LocalDate;
+
+public class Pet {
+
+  private Owner owner;
+  private PetType petType;
+  private LocalDate birthDay;
+
+
+  public Owner getOwner() {
+    return owner;
+  }
+
+  public void setOwner(Owner owner) {
+    this.owner = owner;
+  }
+
+  public PetType getPetType() {
+    return petType;
+  }
+
+  public void setPetType(PetType petType) {
+    this.petType = petType;
+  }
+
+  public LocalDate getBirthDay() {
+    return birthDay;
+  }
+
+  public void setBirthDay(LocalDate birthDay) {
+    this.birthDay = birthDay;
+  }
+}
