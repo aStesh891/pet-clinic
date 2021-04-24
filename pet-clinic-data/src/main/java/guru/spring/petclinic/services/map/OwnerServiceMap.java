2149195e8baa98a7ab6976801668b2539021c09e
aStesh891 <ann.steshhh@gmail.com>
2021-04-24 22:05:01 +0300

    adding bootstrap for data. closes #14

--- pet-clinic-data/src/main/java/guru/spring/petclinic/services/map/OwnerServiceMap.java
+++ pet-clinic-data/src/main/java/guru/spring/petclinic/services/map/OwnerServiceMap.java
@@ -1,13 +1,13 @@
 package guru.spring.petclinic.services.map;
 
 import guru.spring.petclinic.model.Owner;
-import guru.spring.petclinic.services.CrudService;
+import guru.spring.petclinic.services.OwnerService;
 import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
 @Service
-public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements CrudService<Owner, Long> {
+public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements OwnerService {
 
   @Override
   public Set<Owner> findAll() {
@@ -32,5 +32,10 @@ public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements 
   @Override
   public Owner findById(Long id) {
     return super.findById(id);
+  }
+
+  @Override
+  public Owner findByLastName(String lastName) {
+    return null; //todo
   }
 }
---
+++ pet-clinic-web/src/main/java/guru/spring/petclinic/bootstrap/DataLoader.java
@@ -0,0 +1,54 @@
+package guru.spring.petclinic.bootstrap;
+
+import guru.spring.petclinic.model.Owner;
+import guru.spring.petclinic.model.Vet;
+import guru.spring.petclinic.services.OwnerService;
+import guru.spring.petclinic.services.VetService;
+import guru.spring.petclinic.services.map.OwnerServiceMap;
+import guru.spring.petclinic.services.map.VetMapService;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataLoader implements CommandLineRunner {
+
+  private final OwnerService ownerService;
+  private final VetService vetService;
+
+  public DataLoader() {
+    this.ownerService = new OwnerServiceMap();
+    this.vetService = new VetMapService();
+  }
+
+
+  @Override
+  public void run(String... args) throws Exception {
+    Owner owner1 = new Owner();
+    owner1.setFirstName("Anna");
+    owner1.setLastName("Steshenko");
+
+    ownerService.save(owner1);
+
+    Owner owner2 = new Owner();
+    owner2.setFirstName("Irina");
+    owner2.setLastName("Glenanne");
+
+    ownerService.save(owner2);
+
+    System.out.println("Loaded Owners....");
+
+    Vet vet1 = new Vet();
+    vet1.setFirstName("Sam");
+    vet1.setLastName("Axe");
+
+    vetService.save(vet1);
+
+    Vet vet2 = new Vet();
+    vet2.setFirstName("Oscar");
+    vet2.setLastName("Porter");
+
+    vetService.save(vet2);
+
+    System.out.println("Loaded Vets....");
+  }
+}
