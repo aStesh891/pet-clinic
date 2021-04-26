51d278db8305a4d8f8b543a6956f267c4e4aaaa1
aStesh891 <ann.steshhh@gmail.com>
2021-04-26 17:47:05 +0300

    adding owners list and updating controller. closes #16

--- pet-clinic-web/src/main/java/guru/spring/petclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/guru/spring/petclinic/bootstrap/DataLoader.java
@@ -21,12 +21,14 @@ public class DataLoader implements CommandLineRunner {
   @Override
   public void run(String... args) throws Exception {
     Owner owner1 = new Owner();
+    owner1.setId(1L);
     owner1.setFirstName("Anna");
     owner1.setLastName("Steshenko");
 
     ownerService.save(owner1);
 
     Owner owner2 = new Owner();
+    owner2.setId(2L);
     owner2.setFirstName("Irina");
     owner2.setLastName("Glenanne");
 
