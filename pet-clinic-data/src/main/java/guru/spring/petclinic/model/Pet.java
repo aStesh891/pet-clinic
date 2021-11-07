88aea67a290dfe64350b6eea559e0684841d0711
aStesh891 <ann.steshhh@gmail.com>
2021-11-07 20:13:53 +0200

    added create and update pet. closes #56

--- pet-clinic-data/src/main/java/guru/spring/petclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/guru/spring/petclinic/model/Pet.java
@@ -33,4 +33,16 @@ public class Pet extends BaseEntity{
   @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
   private Set<Visit> visits = new HashSet<>();
 
+  @Builder
+  public Pet(Long id, String name, PetType petType, Owner owner, LocalDate birthDate, Set<Visit> visits) {
+    super(id);
+    this.name = name;
+    this.petType = petType;
+    this.owner = owner;
+    this.birthDate = birthDate;
+
+    if (visits == null || visits.size() > 0 ) {
+      this.visits = visits;
+    }
+  }
 }
